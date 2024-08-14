0dd930ac32f4fd2fc2d4187f2788e13eacbdeb93
Alejandra-Diaz28 <160366614+Alejandra-Diaz28@users.noreply.github.com>
2024-08-14 02:39:56 +0000

    hola

--- challenges/lists/main.py
+++ challenges/lists/main.py
@@ -1,4 +1,23 @@
 if __name__ == '__main__':
-    
+n = int(input())
+lista=[]
+for i in range(0,n):
+  comando=input("inserta tu comando_ejemplo: insert 0 5")
+  comando=comando.split(" ")
+  if comando[0]=="insert":
+    lista.insert(int(comando[1]),int(comando[2]))
+  elif comando[0]=="append":
+    lista.append(int(comando[1]))
+  elif comando[0]=="remove":
+    lista.remove(int(comando[1]))
+  elif comando[0]=="reverse":
+    lista.reverse()
+  elif comando[0]=="pop":
+    lista.pop()
+  elif comando[0]=="sort":
+    lista.sort()
+  elif comando[0]=="print":
+    print(lista)
     N = int(input())
+    
     
